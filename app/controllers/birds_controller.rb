a35a3891d4ff46dbc2e80b113ccb6b0fca81837a
Connie Park <conniedc1206@gmail.com>
2022-07-18 18:55:00 -0700

    Finished code-along

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -44,6 +44,21 @@ class BirdsController < ApplicationController
     end
   end
 
+  # DELETE /birds/:id
+  def destroy
+    # Find a bird using the ID from the route params
+    bird = Bird.find_by(id: params[:id])
+    # Remove it from the database with bird.destroy
+    # :no_content will give a 204 status code, indicating that the server has successfully fulfilled the request and that there is no content to send in the response. 
+    if bird
+      bird.destroy
+      head :no_content
+      # render json: {} # json-server handles a successful delete request by sending an empty object
+    else
+      render json: {error: "Bird not found"}, status: :not_found
+    end
+  end
+
   private
 
   def bird_params
